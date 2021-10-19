75384e5d1c7c0123ac7fdda2bb2b69a7984ea475
Nilesh-424105 <89961130+Nilesh-424105@users.noreply.github.com>
2021-10-19 22:22:20 +0530

    ADDED: Welcome Message

---
+++ WelcomeMessage.java
@@ -0,0 +1,10 @@
+package com.line.comparison;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Line Comparison Computation Program");
+	}
+
+}
